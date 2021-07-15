a665e01a0dff5899ec0962da43b7d03b5ee20bdc
chanhooop <cksghwwkd@gmail.com>
2021-07-15 14:55:04 +0900

    day2
    day2

--- Quiz03/Quiz03/ViewController.swift
+++ Quiz03/Quiz03/ViewController.swift
@@ -33,13 +33,35 @@ class ViewController: UIViewController {
         lblResult.text = "입력하신 숫자는 \(message)입니다."
         */
         
-        
-    
+      let numCheck = checkNil(str: tfNum.text!)
+        if numCheck == 1 {
+           let returnValue = numberDeision(str: tfNum.text!)
+            lblResult.text = "입력 하신 숫자는 \(returnValue)입니다."
+        }else{
+            lblResult.text = "숫자를 확인하세요!"
+        }
     
     } //btnEnter
     
-    func  checkNil(str: String!) {
+    
+      //공백찾는 함수  checkNil                        // '->'리턴 타입
+    func  checkNil(str: String!) -> Int {
         let check = str.trimmingCharacters(in: .whitespacesAndNewlines)  // 엔터키와 스페이스를 트리밍해서 정리해준다
-    }
+        if check.isEmpty{
+            return 0
+        }else{
+            return 1
+        }
+    } //checkNil
+    
+    // 계산함수 numberDeision
+    func  numberDeision(str: String!) -> String {
+        if Int(str)! % 2 == 0{
+            return "짝수"
+        }else{
+            return "홀수"
+        }
+    } // numberDeision
+    
 }
 
