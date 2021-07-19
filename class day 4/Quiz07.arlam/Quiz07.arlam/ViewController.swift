f489edc88a4fb0b371f0721f1bf75f3f5699fac7
chanhooop <81682489+chanhooop@users.noreply.github.com>
2021-07-20 03:36:35 +0900

    day4
    day4

--- class day 4/Quiz07.arlam/Quiz07.arlam/ViewController.swift
+++ class day 4/Quiz07.arlam/Quiz07.arlam/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     let interval = 1.0
     let timeselector: Selector = #selector(ViewController.updateTime)
     
+    var alarm = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,30 +25,45 @@ class ViewController: UIViewController {
         Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeselector, userInfo: nil, repeats: true)
 
     }
-    
+    //실시간 현재 시간
     @objc func updateTime(){
         let date = NSDate() // Next Step의 약자
         let formatter = DateFormatter()
+        let alarmFormatter = DateFormatter()
+        var nowAlarm = ""
         
         formatter.locale = Locale(identifier: "ko")
-        formatter.dateFormat = "yyyy-MM-dd,EEE a hh:mm"   //EEE 는 요일
+        formatter.dateFormat = "yyyy-MM-dd EEE a hh:mm:ss"   //EEE 는 요일
         lblCurruntTime.text = "현재시간: \(formatter.string(from: date as Date))"
+        
+        alarmFormatter.locale = Locale(identifier:  "ko")
+        alarmFormatter.dateFormat = "yyyy-MM-dd EEE a hh:mm"
+        nowAlarm = "\(alarmFormatter.string(from: date as Date))"
+        
+        if nowAlarm == alarm {
+            let sec:Int = Int(formatter.string(from: date as Date).suffix(2)) ?? 0
+            switch sec % 2 == 0 {
+            case true:
+                view.backgroundColor = UIColor.red
+            default:
+                view.backgroundColor = UIColor.blue
+            }
+        }else{
+            view.backgroundColor = UIColor.white
+        }
+
     }
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
         let datePickerView = sender
-        //class 생성s
         let formatter = DateFormatter()
-        
+        // 선택한 시간 정의
         formatter.locale = Locale(identifier: "ko")
         formatter.dateFormat = "yyyy-MM-dd EEE a hh:mm" // 24시간용 시간은 HH
         lblPickerTime.text = "선택시간 : \(formatter.string(from: datePickerView.date))"
+        alarm = "\(formatter.string(from: datePickerView.date))"
         
-        if updateTime() == datePickerView.date {
-            view.backgroundColor = UIColor.red
-        }else{
-            view.backgroundColor = UIColor.white
-        }
+
         
         
     }
